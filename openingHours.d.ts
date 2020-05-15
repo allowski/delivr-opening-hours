807575df07509c8e660035d6cdfacb443763fdb8
Alberto Miranda Dencowski <allowski@gmail.com>
2020-05-14 22:14:23 -0300

    Fixed types

--- openingHours.d.ts
+++ openingHours.d.ts
@@ -1,9 +1,11 @@
 export class OpeningHour {
-    _id: string;
-    dayOfWeek: number;
-    hours: {
-        _id: string;
-        from: number;
-        to: number;
-    }[];
+    _id?: string;
+    from?: number;
+    to?: number;
+}
+
+export class OpeningHours {
+    _id?: string;
+    dayOfWeek?: number;
+    hours?: OpeningHour[];
 }
